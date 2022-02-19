4fabe14f74cf50db157da7c9ee1a36ba7377bedf
adechan <ade.enter@yahoo.com>
2022-02-19 21:46:57 +0200

    + cleanup

--- mentor-app-frontend/src/index.js
+++ mentor-app-frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import store from "./store/store"
 
 import { Provider } from 'react-redux'
@@ -17,5 +16,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-
-reportWebVitals();
